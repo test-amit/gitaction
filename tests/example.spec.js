1cc07798b013f89b7174e0149a717dc9e4dfb7c8
test-amit <testamitnayak@gmail.com>
2025-03-19 00:29:02 +0530

    28

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -5,10 +5,12 @@ test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   console.log('First 12:20 PM')
+  console.log('Second 12:28 PM')
 });
 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   console.log('Second 12:20 PM')
+  console.log('Second 12:28 PM')
 });
