e4ccf032ee326338930ed877ceaf5de690e4471e
test-amit <testamitnayak@gmail.com>
2025-03-19 01:07:51 +0530

    /

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -4,29 +4,11 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
-  console.log('First 12:20 PM')
-  console.log('Second 12:28 PM')
-
-  console.log('Second 12:38 PM')
-  console.log('Second 12:50 PM')
+ console.log("this is final Action form our side -> Script First");
 });
 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-
-  console.log('Second 12:20 PM')
-  console.log('Second 12:28 PM')
-
-  console.log('Second 12:38 PM')
-  console.log('Second 12:45 PM')
-
-
-
-
-  console.log('Second 12:50 PM')
-
-  console.log('01 Second final PM')
-
-  console.log('Second 12:5678')
-  console.log('Second 12:50 PM')
+  console.log("this is final Action form our side -> Script Second");
+ 
 });
