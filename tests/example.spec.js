485195dfde222b1541f848788e4400fa23678c9e
test-amit <testamitnayak@gmail.com>
2025-03-20 01:07:14 +0530

    CI/CD

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -16,3 +16,28 @@ test('get started link', async ({ page }) => {
   console.log("Ajit From Odisha And GitAction Process is done with Script Second Checking again")
   
 });
+
+
+
+// name: hello-world
+// on: push
+// jobs:
+//   my-job:
+//     runs-on: macos-latest
+//     steps:
+//       - name: Checkout repository
+//         uses: actions/checkout@v3
+
+//       - name: Setup Node.js
+//         uses: actions/setup-node@v3
+//         with:
+//           node-version: 18
+
+//       - name: Install dependencies
+//         run: npm install
+
+//       - name: Install Playwright Browsers
+//         run: npx playwright install --with-deps
+
+//       - name: Run Playwright tests
+//         run: npx playwright test example.spec.js --workers=1 --project=chromium --reporter=dot
