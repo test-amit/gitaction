1c8c37b1aa2c838902fd0814e960d9b584b31fc0
test-amit <testamitnayak@gmail.com>
2025-03-20 00:58:55 +0530

    checking again

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -5,11 +5,14 @@ test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
 console.log("Ajit From Odisha And GitAction Process is done with Script First")
+
+console.log("Ajit From Odisha And GitAction Process is done with Script First Checking again")
 });
 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
-  console.log("Ajit From Odisha And GitAction Process is done with Script Second")
+  console.log("Ajit From Odisha And GitAction Process is done with Script Second ")
+  console.log("Ajit From Odisha And GitAction Process is done with Script Second Checking again")
   
 });
