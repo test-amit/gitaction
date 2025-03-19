dce21a9f635474f8048772f299d83c6945e948da
test-amit <testamitnayak@gmail.com>
2025-03-20 00:54:49 +0530

    final version

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -4,11 +4,12 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
- console.log("this is final Action form our side -> Script First");
+console.log("Ajit From Odisha And GitAction Process is done with Script First")
 });
 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
+
+  console.log("Ajit From Odisha And GitAction Process is done with Script Second")
   
-   console.log("master branch -3");
 });
